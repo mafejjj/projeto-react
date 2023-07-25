61cef128ffc59688c0cd013137f7a57bd24bdb80
Maria Fernanda de Jesus <mariafernandadejesus28@gmail.com>
2023-07-25 14:25:50 -0300

    instrodução react-js

---
+++ src/components/Item.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+
+function Item({marca, ano_lancamento}){
+    return(
+        <>
+            <li>
+            {marca} - {ano_lancamento}
+            </li>
+        </>
+    )
+}
+
+Item.PropTypes = {
+    marca: PropTypes.string.isRequired,
+    ano_lancamento: PropTypes.number,
+}
+
+Item.defaultProps = {
+    marca:'faltou a marca',
+    ano_lancamento: 0,
+}
+export default Item
---
+++ src/pages/Empresa.js
@@ -0,0 +1,12 @@
+function empresa() {
+
+    return(
+        <div>
+            <h1>Empresa</h1>
+            <p>Conteúdo da página</p>
+        </div>
+    )
+
+}
+
+export default empresa
